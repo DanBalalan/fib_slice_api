2a5c9dddc279e1efa585df012d251dffde8e6e4e
d.skrynnik@pressfoto.ru <bbcktpassw0rd>
2022-03-11 13:15:49 +0500

    refactor FibonacciView; process request args

--- views/fibonacci.py
+++ views/fibonacci.py
@@ -1,5 +1,4 @@
-from itertools import count
-
+from flask import request
 from flask.views import MethodView
 
 from utils.fibonacci import fibonacci
@@ -8,15 +7,8 @@ from utils.fibonacci import fibonacci
 class FibonacciView(MethodView):
 
     def get(self):
-        # TODO: форму
-        return ', '.join(str(i) for i in self.__get_fib())
-
-    def __get_fib(self, min_num=0, max_num=50):
-        res = []
-        for i in count(0):
-            curr_fib_num = fibonacci(position=i)
-            if curr_fib_num > max_num:
-                break
-            else:
-                res.append(curr_fib_num)
-        return res
+        from_ = int(request.args.get('from', 0))
+        to_ = int(request.args.get('to', 0))
+        if from_ > to_ or from_ < 0 or to_ < 0:
+            return ''
+        return ', '.join(str(fibonacci(i)) for i in range(from_, to_ + 1, 1))
