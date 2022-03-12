69b7ccadc9ca7fc34975ec690b0bc75161d82dcb
d.skrynnik@pressfoto.ru <bbcktpassw0rd>
2022-03-12 17:14:33 +0500

    refactor: all fib related funcs to utils.fibonacci

--- views/fibonacci/views.py
+++ views/fibonacci/views.py
@@ -4,8 +4,7 @@ from itertools import count
 from flask import request, render_template
 from flask.views import MethodView
 
-from utils.binary_search import binary_search_nearest
-from utils.fibonacci import fibonacci
+from utils.fibonacci import get_by_position_range, get_by_value_range
 from utils.redis_client import RedisClient
 from settings import CACHE_KEY_PREFIX
 from .forms import FibonacciForm
@@ -25,11 +24,11 @@ class FibonacciView(MethodView):
         }
         if form.validate():
             context['result'] = {
-                'pos': self.__get_by_position(
+                'pos': get_by_position_range(
                     start_pos=form.from_arg.data,
                     stop_pos=form.to_arg.data,
                 ),
-                'val': self.__get_by_value(
+                'val': get_by_value_range(
                     min_value=form.from_arg.data,
                     max_value=form.to_arg.data,
                 ),
@@ -37,73 +36,3 @@ class FibonacciView(MethodView):
         else:
             context['errors'].append(form.errors)
         return render_template(self.template, context=context)
-
-    def __get_by_value(self, min_value, max_value):
-        if min_value > max_value:
-            return []
-
-        # Начинаем с максимально возможного значения, попутно заполняя весь недостающий кеш
-        # (предполагая, что существующий кеш непрерывный и целостный)
-        max_cached_pos, max_cached_value = self.__get_cached_max_position()
-        min_pos = 0
-        if min_value > max_cached_value:
-            min_pos = max_cached_pos + 1
-        elif min_value == max_cached_value:
-            min_pos = max_cached_pos
-        else:  # min_value < max_cached_value
-            positions_sequence = [i for i in range(0, max_cached_pos, 1)]
-            min_pos, exact = binary_search_nearest(positions_sequence, min_value, fibonacci)
-            if not exact and min_pos > 0:
-                min_pos -= 1
-
-        res = []
-        for i in count(min_pos, 1):
-            fib_by_pos = fibonacci(i)
-            if min_value <= fib_by_pos <= max_value:
-                res.append(fib_by_pos)
-            elif fib_by_pos > max_value:
-                break
-            else:
-                continue
-        return res
-
-    def __get_by_position(self, start_pos, stop_pos):
-        if start_pos > stop_pos:
-            return []
-
-        # Начинаем с максимально возможного значения, попутно заполняя весь недостающий кеш
-        # (предполагая, что существующий кеш непрерывный и целостный)
-        # start_iter_pos - для заполнения кеша при отсутсвии какой-либо части его диапазона
-        # start_pos - начальная искомая позиция, значение в которой отдаётся наружу
-        cached_max, _ = self.__get_cached_max_position()
-        start_iter_pos = start_pos
-        if start_pos > cached_max:
-            start_iter_pos = cached_max + 1
-
-        curr_max_pos, curr_max_val = 0, 0
-        res = []
-        for i in range(start_iter_pos, stop_pos + 1, 1):
-            fib_by_pos = fibonacci(i)
-            if i >= start_pos:
-                res.append(fib_by_pos)
-            curr_max_pos, curr_max_val = i, fib_by_pos
-
-        self.__cache_max_position(curr_max_pos, curr_max_val)
-
-        return res
-
-    def __get_cached_max_position(self):
-        cached = self.redis_client.get(self.max_position_key)
-        if cached:
-            pos, val = json.loads(cached)
-        else:
-            pos, val = 0, 0
-            self.redis_client.set(self.max_position_key, json.dumps((pos, val)))
-            fibonacci(pos)  # Создаём кеш от аргументов функции
-        return pos, val
-
-    def __cache_max_position(self, pos, val):
-        cached_pos, cached_val = self.__get_cached_max_position()
-        if cached_pos > pos and cached_val > val:
-            return
-        self.redis_client.set(self.max_position_key, json.dumps((pos, val)))
